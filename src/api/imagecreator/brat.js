005a98fd89ed69159b149c4c4b752aacaab0f709
rijalganzz <keyshan430@gmail.com>
2025-07-17 23:06:50 +0700

    Add files via upload

--- src/api/imagecreator/brat.js
+++ src/api/imagecreator/brat.js
@@ -1,8 +1,7 @@
-module.exports = function app (app) {
-app.get('/imagecreator/brat', async (req, res) => {
+module.exports = function app(app) {
+    app.get('/imagecreator/brat', async (req, res) => {
         try {
-            const { apikey, text } = req.query
-            if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
+            const { text } = req.query
             const pedo = await getBuffer(`https://aqul-brat.hf.space/?text=${encodeURIComponent(text)}&mode=image`)
             res.writeHead(200, {
                 'Content-Type': 'image/png',
@@ -14,18 +13,17 @@ app.get('/imagecreator/brat', async (req, res) => {
         }
     })
 
-app.get('/imagecreator/bratvideo', async (req, res) => {
+    app.get('/imagecreator/bratvideo', async (req, res) => {
         try {
-            const { apikey, text } = req.query
-            if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
+            const { text } = req.query
             const pedo = await getBuffer(`https://fastrestapis.fasturl.cloud/maker/brat/animated?text=${text}&mode=animated`)
             res.writeHead(200, {
                 'Content-Type': 'video/mp4',
-                'Content-Length': pedo.length,
-            });
-            res.end(pedo);
+                'Content-Length': pedo.length
+            })
+            res.end(pedo)
         } catch (error) {
-            res.status(500).send(`Error: ${error.message}`);
+            res.status(500).send(`Error: ${error.message}`)
         }
-    });    
+    })
 }
