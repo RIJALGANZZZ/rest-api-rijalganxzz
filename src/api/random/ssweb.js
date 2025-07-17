e729da46f54530e2b2f523b2b857dea7b6e44c2b
rijalganzz <keyshan430@gmail.com>
2025-07-17 23:24:50 +0700

    Add files via upload

--- src/api/random/ssweb.js
+++ src/api/random/ssweb.js
@@ -1,7 +1,6 @@
 module.exports = function(app) {
 app.get('/random/ssweb', async (req, res) => {
-       const { apikey, url } = req.query
-       if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
+       const { url } = req.query
        if (!url) return res.json({ status: false, error: 'Url is required' });
         try {
             let anu = await fetchJson(`https://api.pikwy.com/?tkn=125&d=3000&u=${url}&fs=0&w=1280&h=1200&s=100&z=100&f=$jpg&rt=jweb`)
